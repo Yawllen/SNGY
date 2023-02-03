d63e9d7a91fe0afed096c5e9504f96ed7398eabd
Даниил Ешенко <91437231+Yawllen@users.noreply.github.com>
2023-02-03 16:39:20 +0300

    приветствие по ФИ в телеграмме

--- bot.py
+++ bot.py
@@ -4,23 +4,31 @@ import config
 import sqlite3
 import time
 
-
 salt = b"yawllen"
 bot = telebot.TeleBot(config.TOKEN)
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Привет, " + message.from_user.username + "! ")
-    time.sleep(1)
-    bot.send_message(message.chat.id, "Для того, чтобы получать и отправлять сообщения, нужно провести аутентификацию на сайте.")
-    time.sleep(1)
-    login = bot.send_message(message.chat.id, 'Введите логин')
-    bot.register_next_step_handler(login, step_Set_Login)
+    name = message.from_user.first_name
+    secondName = message.from_user.last_name
+    if secondName is not None:
+        bot.send_message(message.chat.id, "Здравствуйте, " + name + " " + secondName + "! ")
+        time.sleep(1)
+        bot.send_message(message.chat.id, "Для того, чтобы получать и отправлять сообщения, нужно провести аутентификацию на сайте.")
+        time.sleep(1)
+        login = bot.send_message(message.chat.id, 'Введите логин')
+        bot.register_next_step_handler(login, step_Set_Login)
+    else:
+        bot.send_message(message.chat.id, "Привет, " + name + "! ")
+        time.sleep(1)
+        bot.send_message(message.chat.id, "Для того, чтобы получать и отправлять сообщения, нужно провести аутентификацию на сайте.")
+        time.sleep(1)
+        login = bot.send_message(message.chat.id, 'Введите логин')
+        bot.register_next_step_handler(login, step_Set_Login)
 
 def step_Set_Login(message):
     global userLogin
     userLogin= message.text
-    time.sleep(1)
-    bot.send_message(message.chat.id, "Ваш логин: " + userLogin)
     time.sleep(1)
     password = bot.send_message(message.chat.id, "Введите пароль")
     bot.register_next_step_handler(password, step_Set_Password)
@@ -36,36 +44,32 @@ def send_link(message):
     bot.send_message(message.chat.id, "@PractInSynerBot")
 
 def check(message):
-
-        loginForCheck = str(userLogin)
-
-        # bot.send_message(message.chat.id, "userLogin:  " + loginForCheck)
-        conn = sqlite3.connect('sup.sqlite')
-        cur = conn.cursor()
-        cur.execute(f"SELECT * FROM AUTH WHERE login = '{loginForCheck}'")
-        record = cur.fetchone()
-        if record:
-            if record[1] == loginForCheck:
-                if record[2] == str(key):
-                    time.sleep(1)
-                    bot.send_message(message.chat.id, "Добро пожаловать, " + record[3] + '!')
-                    time.sleep(1)
-                    bot.send_message(message.chat.id, "Ваш ID: " + str(record[0]))
-                else:
-                    cur.close()
-                    time.sleep(1)
-                    bot.send_message(message.chat.id, "Логин или пароль неверный 1")
-                    send_welcome(message)
+    loginForCheck = str(userLogin)
+    conn = sqlite3.connect('sup.sqlite')
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM AUTH WHERE login = '{loginForCheck}'")
+    record = cur.fetchone()
+    if record:
+        if record[1] == loginForCheck:
+            if record[2] == str(key):
+                time.sleep(1)
+                bot.send_message(message.chat.id, "Добро пожаловать, " + record[3] + '!')
+                time.sleep(1)
+                bot.send_message(message.chat.id, "Ваш ID: " + str(record[0]))
             else:
                 cur.close()
                 time.sleep(1)
-                bot.send_message(message.chat.id, "Логин или пароль неверный 2")
+                bot.send_message(message.chat.id, "Логин или пароль неверный 1")
                 send_welcome(message)
         else:
             cur.close()
             time.sleep(1)
-            bot.send_message(message.chat.id, "Логин или пароль неверный 3")
+            bot.send_message(message.chat.id, "Логин или пароль неверный 2")
             send_welcome(message)
-
+    else:
+        cur.close()
+        time.sleep(1)
+        bot.send_message(message.chat.id, "Логин или пароль неверный 3")
+        send_welcome(message)
 
 bot.polling(none_stop=True)
