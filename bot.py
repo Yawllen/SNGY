dfa580bdfe74da550b4ab4bcee45909581a7c310
Даниил Ешенко <91437231+Yawllen@users.noreply.github.com>
2023-02-07 13:33:37 +0300

    7
    1. Добавлен файл requirements_all.txt, в котором автоматически сформирован список всех пакетов, импортированных в проект. Но тк алгоритм действий несколько раз менялся, некоторые пакеты из этого списка не использовались.
    2. Добавление файла requirements_base.txt, в котором вручную написан пакет, который необходимо было дополнительно импортировать для работы с телеграмм ботом.
    3. Добавление докстринга.

--- bot.py
+++ bot.py
@@ -10,6 +10,14 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    """
+    Метод, запускающийся при старте бота. Принимает входящее сообщение.
+    Метод проверяет: сообщение пришло боту из лс или из группы, для каждого варианта свой алгоритм действий.
+    Если боту написали в лс, он переходит к методу аутентификации.
+    Если боту пишут в группе:
+        1. Проверка на админа.
+        2. Получение информации о группе (id группы и номер проекта, по которому в БД находится id соответствующего проекта).
+    """
     if(message.chat.id > 0):  #личка
         welcome(message)
     else:                   #группа
@@ -25,11 +33,18 @@ def send_welcome(message):
                 numProj = str(re.sub(r'[^!?,.\d]+', '', str(title[1])))
                 @bot.message_handler(commands=['proj'])
                 def admin(message):
+                    """
+                    По команде /proj бот выводит номер нашего проекта исходя из названия группы.
+                    """
                     time.sleep(1)
                     bot.send_message(message.chat.id, "Номер вашего проекта: " + numProj)
                     step_Set_Project(message)
                 def step_Set_Project(message):
-
+                    """
+                    1. Получение id проекта из БД по номеру проекта.
+                    2. Заполнение id группы по id проекта в БД.
+                    3. Бот выводит ссылку на себя для перехода в лс.
+                    """
                     conn = sqlite3.connect('sup.sqlite')
                     cur = conn.cursor()
                     cur.execute(f"SELECT * FROM PROJ_SUP WHERE num_project = '{numProj}'")
@@ -42,16 +57,14 @@ def send_welcome(message):
                         cur.execute(f"SELECT * FROM GROUPP WHERE id_project  = '{idProj}'")
                         record2 = cur.fetchone()
                         if record2:
-                            if record2[1] is None:
+                            if record2[1]:
                                 # bot.send_message(message.chat.id, "Обновление данных")
                                 cur.execute(
-                                    f"UPDATE GROUPP SET  id_group  = '{message.chat.id}' WHERE id_project  = '{idProj}' ")
+                                    f"UPDATE GROUPP SET id_group  = '{message.chat.id}' WHERE id_project  = '{idProj}' ")
                                 conn.commit()
                             else:
-                                # bot.send_message(message.chat.id, "Выход 1")
                                 cur.close()
                         else:
-                            # bot.send_message(message.chat.id, "Выход 2")
                             cur.close()
 
                         time.sleep(1)
@@ -63,12 +76,15 @@ def send_welcome(message):
                         bot.send_message(message.chat.id, "Записи не найдены")
                         admin(message)
 def welcome(message):
+    """
+    Приветствие и аутентификация пользователя в лс
+    """
     name = message.from_user.first_name
     secondName = message.from_user.last_name
     if secondName is not None:
         bot.send_message(message.chat.id, "Здравствуйте, " + name + " " + secondName + "! ")
         time.sleep(1)
-        bot.send_message(message.chat.id, "Для того, чтобы получать и отправлять сообщения, нужно провести аутентификацию на сайте.")
+        bot.send_message(message.chat.id, "Для того, чтобы получать и отправлять сообщения, нужно произвести авторизацию на сайте.")
         time.sleep(1)
         login = bot.send_message(message.chat.id, 'Введите логин')
         bot.register_next_step_handler(login, step_Set_Login)
@@ -81,6 +97,9 @@ def welcome(message):
         bot.register_next_step_handler(login, step_Set_Login)
 
 def step_Set_Login(message):
+    """
+    Проверка логина пользователя и ввод пароля
+    """
     global userLogin
     userLogin = message.text
     time.sleep(1)
@@ -88,12 +107,23 @@ def step_Set_Login(message):
     bot.register_next_step_handler(password, step_Set_Password)
 
 def step_Set_Password(message):
+    """
+    Хеширование пароля с солью
+    """
     global key
     key = hashlib.pbkdf2_hmac('sha256', message.text.encode('utf-8'), salt, 100000)
-    # bot.send_message(message.chat.id, "Ваш пароль: " + password)
     check(message)
 
 def check(message):
+    """
+    Проверка введённых данных пользователя по данным СУП.
+    Если проверка прошла успешно:
+        1. Приветствие пользователя по его имени из СУП.
+        2. Вывод id пользователя из СУП.
+        3. Сравниваем id пользователя из СУП с id пользователя из БД.
+        4. Если такой записи нет в БД, добавляем её.
+    Если проверка не пройдена, пользователя просят ввести данные повторно.
+    """
     loginForCheck = str(userLogin)
     conn = sqlite3.connect('sup.sqlite')
     cur = conn.cursor()
@@ -115,26 +145,26 @@ def check(message):
                     cur.execute(f"INSERT INTO CHAT (id_user, id_chat)  VALUES (?, ?)", (userID, message.chat.id))
                     conn.commit()
                     time.sleep(1)
-                    bot.send_message(message.chat.id, "Авторизация прошла успешно 1!")
+                    bot.send_message(message.chat.id, "Авторизация прошла успешно!")
                     cur.close()
                 else:
                     time.sleep(1)
-                    bot.send_message(message.chat.id, "Авторизация прошла успешно 2!")
+                    bot.send_message(message.chat.id, "Авторизация прошла успешно!")
                     cur.close()
             else:
                 cur.close()
                 time.sleep(1)
-                bot.send_message(message.chat.id, "Логин или пароль неверный 1")
+                bot.send_message(message.chat.id, "Логин или пароль неверный")
                 send_welcome(message)
         else:
             cur.close()
             time.sleep(1)
-            bot.send_message(message.chat.id, "Логин или пароль неверный 2")
+            bot.send_message(message.chat.id, "Логин или пароль неверный")
             send_welcome(message)
     else:
         cur.close()
         time.sleep(1)
-        bot.send_message(message.chat.id, "Логин или пароль неверный 3")
+        bot.send_message(message.chat.id, "Логин или пароль неверный")
         send_welcome(message)
 
 
